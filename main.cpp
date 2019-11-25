e253989674421a0c89856520b5d1599736009510
#kuma <hj-g18004@sist.ac.jp>
2019-11-25 16:29:00 +0900

    ひな形作成途中

---
+++ main.cpp
@@ -0,0 +1,316 @@
+#include "DxLib.h"
+
+#define GAME_WIDTH	560
+#define GAME_HEIGHT 625
+#define GAME_COLOR	32
+
+#define GAME_WINDOW_NAME		"TETRIS_REVERSE"
+#define GAME_WINDOW_MODECHANGE	TRUE	//TRUE:ウィンドウモード　FALSE:フルスクリーン
+
+#define SET_WINDOW_ST_MODE_DEFAULT			0
+#define SET_WINDOW_ST_MODE_TITLE_NONE		1
+#define SET_WINDOW_ST_MODE_TITLE_FLAME_NONE	2
+#define SET_WINDOW_ST_MODE_FLAME_NONE		3
+
+#define GAME_FPS_SPEED	60
+
+#define NONE	0
+#define ENDLESS	1
+#define TIME	2
+
+enum GAME_SCENE {
+	GAME_SCENE_TITLE,		//タイトル画面
+	GAME_SCENE_RANKING,		//ランキング画面
+	GAME_SCENE_SOZAI,		//素材表示画面
+	GAME_SCENE_PLAY_ENDLESS,//プレイ画面(エンドレスモード)
+	GAME_SCENE_PLAY_TIME,	//プレイ画面(タイムアタックモード)
+	GAME_SCENE_CHECK,		//確認画面
+	GAME_SCENE_END_CLEAR,	//エンド画面(ゲームクリア)
+	GAME_SCENE_END_OVER		//エンド画面(ゲームオーバー)
+};
+
+
+BOOL IsWM_CREATE = FALSE;	//WM_CREATEが正常に動作したか判断する
+
+int StartTimeFps;					//測定開始時刻
+int CountFps;						//カウンタ
+float CalcFps;						//計算結果
+int SampleNumFps = GAME_FPS_SPEED;	//平均を取るサンプル数
+
+char AllKeyState[256];	//全てのキーの状態が入る
+
+int GameSceneNow = (int)GAME_SCENE_TITLE;	//最初のゲーム画面をタイトルに設定
+
+int playmode = NONE;
+
+LRESULT CALLBACK MY_WNDPROC(HWND, UINT, WPARAM, LPARAM);	//自作ウィンドウプロシージャ
+
+VOID MY_FPS_UPDATE(VOID);	//FPSを計測、更新する関数
+VOID MY_FPS_WAIT(VOID);		//FPS値を計測し、待つ関数
+
+VOID MY_ALL_KEYDOWN_UPDATE(VOID);	//キーの入力状態を更新する関数
+
+VOID MY_GAME_TITLE(VOID);		//タイトル
+VOID MY_GAME_RANKING(VOID);		//ランキング
+VOID MY_GAME_SOZAI(VOID);		//素材表示
+VOID MY_GAME_PLAY_ENDLESS(VOID);//プレイ・エンドレス
+VOID MY_GAME_PLAY_TIME(VOID);	//プレイ・タイムアタック
+VOID MY_GAME_CHECK(VOID);		//確認
+VOID MY_GAME_END_CLEAR(VOID);	//ゲームクリア
+VOID MY_GAME_END_OVER(VOID);	//ゲームオーバー
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(GAME_WINDOW_MODECHANGE);	//ウィンドウモードに設定
+	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);	//指定の数値で画面を表示する
+
+	SetWindowStyleMode(SET_WINDOW_ST_MODE_DEFAULT);	//タイトルバー
+
+	SetMainWindowText(TEXT(GAME_WINDOW_NAME));		//タイトルの文字
+
+	if (DxLib_Init() == -1)	{ return -1; }	//DxLib初期化処理
+
+	SetDrawScreen(DX_SCREEN_BACK);	//Draw系関数は裏画面に描画
+
+	while (TRUE)
+	{
+		if (ProcessMessage() != 0) { break; }	//メッセージ処理の結果がエラーの時、強制終了
+
+		if (ClearDrawScreen() != 0) { break; }	//画面を消去出来なかった時、強制終了
+	
+		MY_ALL_KEYDOWN_UPDATE();
+		//次ここから
+	}
+}
+
+//FPSを計測、更新する関数
+VOID MY_FPS_UPDATE(VOID)
+{
+	if (CountFps == 0)
+	{
+		StartTimeFps = GetNowCount();	//測定開始時刻設定
+	}
+
+	if (CountFps == SampleNumFps)
+	{
+		int now = GetNowCount();		//現時刻を取得
+		CalcFps = 1000.f / ((now - StartTimeFps) / (float)SampleNumFps);	//計算
+		CountFps = 0;	//カウントリセット
+		StartTimeFps = now;	//現時刻から測定開始
+	}
+	CountFps++;
+	return;
+}
+
+//FPS値を計測し、待つ関数
+VOID MY_FPS_WAIT(VOID)
+{
+	int resultTime = GetNowCount() - StartTimeFps;	//かかった時間
+	int waitTime = CountFps * 1000 / GAME_FPS_SPEED - resultTime;	//待つべき時間
+
+	//指定のFPS値よりも早い場合
+	if (waitTime > 0)
+	{
+		Sleep(waitTime);	//待つ
+	}
+	return;
+}
+
+//キーの入力状態を更新する関数
+VOID MY_ALL_KEYDOWN_UPDATE(VOID)
+{
+	char TempKey[256];	//一時的に現在のキーの入力状態を格納する
+
+	GetHitKeyStateAll(TempKey);	//全てのキーの入力状態を得る
+
+	for (int i = 0; i < 256; i++)	//押されているキーのキーコードを押しているとき
+	{
+		if (TempKey[i] != 0)	//押されている
+		{
+			AllKeyState[i]++;
+		}
+		else
+		{
+			AllKeyState[i] = 0;	//押されていない
+		}
+	}
+	return;
+}
+
+VOID MY_GAME_TITLE(VOID)
+{
+	playmode = NONE;
+
+	if (AllKeyState[KEY_INPUT_E] != 0)
+	{
+		playmode = ENDLESS;
+		GameSceneNow = (int)GAME_SCENE_PLAY_ENDLESS;
+	}
+	else if (AllKeyState[KEY_INPUT_T] != 0)
+	{
+		playmode = TIME;
+		GameSceneNow = (int)GAME_SCENE_PLAY_TIME;
+	}
+	else if (AllKeyState[KEY_INPUT_R] != 0)
+	{
+		GameSceneNow = (int)GAME_SCENE_RANKING;
+	}
+	else if (AllKeyState[KEY_INPUT_S] != 0)
+	{
+		GameSceneNow = (int)GAME_SCENE_SOZAI;
+	}
+	DrawString(0, 0, "タイトル画面", GetColor(255, 255, 255));
+	DrawString(0, 30, "エンドレスモード：Ｅキー", GetColor(255, 255, 255));
+	DrawString(0, 60, "タイムアタックモード：Ｔキー", GetColor(255, 255, 255));
+	DrawString(0, 90, "ランキング：Ｒキー", GetColor(255, 255, 255));
+	DrawString(0, 120, "素材：Ｓキー", GetColor(255, 255, 255));
+
+	return;
+}
+
+VOID MY_GAME_RANKING(VOID)
+{
+	if (AllKeyState[KEY_INPUT_BACK] != 0)
+	{
+		GameSceneNow = (int)GAME_SCENE_TITLE;
+	}
+
+	DrawString(0, 0, "ランキング画面", GetColor(255, 255, 255));
+	DrawString(0, 30, "タイトルに戻る：ＢＡＣＫＳＰＡＣＥキー", GetColor(255, 255, 255));
+
+	return;
+}
+
+VOID MY_GAME_SOZAI(VOID)
+{
+	if (AllKeyState[KEY_INPUT_BACK] != 0)
+	{
+		GameSceneNow = (int)GAME_SCENE_TITLE;
+	}
+
+	DrawString(0, 0, "素材表示画面", GetColor(255, 255, 255));
+	DrawString(0, 30, "タイトルに戻る：ＢＡＣＫＳＰＡＣＥキー", GetColor(255, 255, 255));
+
+	return;
+}
+
+VOID MY_GAME_PLAY_ENDLESS(VOID)
+{
+	if (AllKeyState[KEY_INPUT_RETURN] != 0)
+	{
+		GameSceneNow = (int)GAME_SCENE_END_OVER;
+	}
+
+	DrawString(0, 0, "プレイ画面(エンドレスモード)", GetColor(255, 255, 255));
+	DrawString(0, 30, "エンド画面：ＥＮＴＥＲキー", GetColor(255, 255, 255));
+
+	return;
+}
+
+VOID MY_GAME_PLAY_TIME(VOID)
+{
+	if (AllKeyState[KEY_INPUT_RETURN] != 0)
+	{
+		GameSceneNow = (int)GAME_SCENE_END_OVER;
+	}
+	else if (AllKeyState[KEY_INPUT_SPACE] != 0)
+	{
+		GameSceneNow = (int)GAME_SCENE_END_CLEAR;
+	}
+
+	DrawString(0, 0, "プレイ画面(エンドレスモード)", GetColor(255, 255, 255));
+	DrawString(0, 30, "エンド画面(ゲームオーバー)：ＥＮＴＥＲキー", GetColor(255, 255, 255));
+	DrawString(0, 60, "エンド画面(ゲームクリア)：ＳＰＡＣＥキー", GetColor(255, 255, 255));
+
+	return;
+}
+
+VOID MY_GAME_CHECK(VOID)
+{
+	if (AllKeyState[KEY_INPUT_Y] != 0)
+	{
+		GameSceneNow = (int)GAME_SCENE_TITLE;
+	}
+	else if (AllKeyState[KEY_INPUT_N] != 0)
+	{
+		if (playmode = ENDLESS)
+		{
+			GameSceneNow = (int)GAME_SCENE_PLAY_ENDLESS;
+		}
+		else if (playmode = TIME)
+		{
+			GameSceneNow = (int)GAME_SCENE_PLAY_TIME;
+		}
+	}
+
+	DrawString(0, 0, "確認画面", GetColor(255, 255, 255));
+	DrawString(0, 0, "ゲームを終了しますか？", GetColor(255, 255, 255));
+	DrawString(0, 0, "タイトル：Ｙキー", GetColor(255, 255, 255));
+	DrawString(0, 0, "プレイを続行：Ｎキー", GetColor(255, 255, 255));
+
+	return;
+}
+VOID MY_GAME_END_OVER(VOID)
+{
+	if (AllKeyState[KEY_INPUT_BACK] != 0)
+	{
+		GameSceneNow = (int)GAME_SCENE_TITLE;
+	}
+	else if (AllKeyState[KEY_INPUT_R] != 0)
+	{
+		GameSceneNow = (int)GAME_SCENE_RANKING;
+	}
+
+	DrawString(0, 0, "エンド画面(ゲームオーバー)", GetColor(255, 255, 255));
+	DrawString(0, 30, "タイトル：ＢＡＣＫＳＰＡＣＥキー", GetColor(255, 255, 255));
+	DrawString(0, 60, "ランキング：Ｒキー", GetColor(255, 255, 255));
+	return;
+}
+
+VOID MY_GAME_END_CLEAR(VOID)
+{
+	if (AllKeyState[KEY_INPUT_BACK] != 0)
+	{
+		GameSceneNow = (int)GAME_SCENE_TITLE;
+	}
+	else if (AllKeyState[KEY_INPUT_R] != 0)
+	{
+		GameSceneNow = (int)GAME_SCENE_RANKING;
+	}
+	
+	DrawString(0, 0, "エンド画面(ゲームオーバー)", GetColor(255, 255, 255));
+	DrawString(0, 30, "タイトル：ＢＡＣＫＳＰＡＣＥキー", GetColor(255, 255, 255));
+	DrawString(0, 60, "ランキング：Ｒキー", GetColor(255, 255, 255));
+
+	return;
+}
+
+LRESULT CALLBACK MY_WNDPROC(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	switch (msg)
+	{
+	case WM_CREATE:
+
+		IsWM_CREATE = TRUE;
+		return 0;
+
+	case WM_CLOSE:
+
+		MessageBox(hwnd, TEXT("ゲームを終了します"), TEXT("終了メッセージ"), MB_OK);
+		break;
+
+	case WM_RBUTTONDOWN:
+		SendMessage(hwnd, WM_CLOSE, 0, 0);
+		break;
+
+	case WM_LBUTTONDOWN:
+		PostMessage(hwnd, WM_NCLBUTTONDOWN, (WPARAM)HTCAPTION, lp);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wp, lp);
+}
